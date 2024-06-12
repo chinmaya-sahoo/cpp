99e53939039459d11a8ea82867f95d24f51f0a7d
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-06-12 20:52:35 +0530

    Files added

--- c++ start/V13Array1.cpp
+++ c++ start/V13Array1.cpp
@@ -1,4 +1,6 @@
+//Vectors -> Dynamic arrays -> resizable(furthur elements can be added or removed) -> also contigous memory like array
 #include<iostream>
+#include<vector>
 using namespace std;
 int main(){
     
