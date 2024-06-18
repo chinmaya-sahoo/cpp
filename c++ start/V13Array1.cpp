75a0c186478e3f12ccbe8c142598b17c889395d8
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-06-18 19:30:45 +0530

    files added

--- c++ start/V13Array1.cpp
+++ c++ start/V13Array1.cpp
@@ -3,6 +3,10 @@
 #include<vector>
 using namespace std;
 int main(){
+    vector<int> v;
+    v.push_back(7);
+    cout<<"size : "<<v.size();
+    cout<<"capacity : "<<v.capacity();
     
     return 0 ;
 }
