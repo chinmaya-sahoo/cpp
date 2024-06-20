aaaa6ae6992e4e035adc9b5d157fa7ba560f7898
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-06-20 20:56:17 +0530

    files added

--- c++ start/V13Array3.cpp
+++ c++ start/V13Array3.cpp
@@ -4,6 +4,15 @@
 using namespace std;
 int main()
 {
-    vector<int>v(5);
+    vector<int>v(6);
+    for (int i = 0; i < 6; i++)
+    {
+        cin>>v[i];
+    }
+    
+    for (int i = 0; i < 6; i++)
+    {
+        cout<<v[i]<<" ";
+    }
     
 }
