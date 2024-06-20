61ffc9e8777ee38f31e862cf5002c3cea080a56b
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-06-20 20:10:30 +0530

    files added

--- c++ start/V13Array2.cpp
+++ c++ start/V13Array2.cpp
@@ -5,16 +5,16 @@ int main()
 {
     vector<int>v;
     // for loop
-    for(int i=0;i<5;i++){
-        int element;
-        cin>>element;
-        v.push_back(element);
-    }
+
+    // for(int i=0;i<5;i++){
+    //     int element;
+    //     cin>>element;
+    //     v.push_back(element);
+    // }
 
     // OR
 
     // vector<int> v(5);
-    // // for loop
     // for (int i = 0; i < 5; i++)
     // {
     //     cin>>v[i];
