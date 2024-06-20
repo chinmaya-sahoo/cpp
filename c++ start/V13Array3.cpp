fe82f1fda66dc83921686bfb7e6ca37d372cdcd8
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-06-20 20:50:57 +0530

    files added

---
+++ c++ start/V13Array3.cpp
@@ -0,0 +1,9 @@
+// find the last occurance(index) of an element x in a given array (try using vector)
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<int>v(5);
+    
+}
