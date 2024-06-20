979591e1b80989202316802ed7e606946d7128b9
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-06-20 21:10:17 +0530

    files added

--- c++ start/V13Array3.cpp
+++ c++ start/V13Array3.cpp
@@ -5,14 +5,27 @@ using namespace std;
 int main()
 {
     vector<int>v(6);
+    cout<<"Enter element : " ;
     for (int i = 0; i < 6; i++)
     {
         cin>>v[i];
     }
     
-    for (int i = 0; i < 6; i++)
+    // for (int i = 0; i < 6; i++)
+    // {
+    //     cout<<v[i]<<" ";
+    // }
+
+    int x,ocr=-1;
+    cout<<"Enter x : " ;
+    cin>>x;//Enter x
+    
+    for (int idx = 0; idx < 6; idx++)
     {
-        cout<<v[i]<<" ";
+        if(v[idx]==x){
+            ocr=idx;
+        }
     }
     
+    cout<<"the last occurance of "<< x << " is : "<< ocr;
 }
