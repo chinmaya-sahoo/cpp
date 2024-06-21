f929d178a62ed1865ef04e9f8f295493ba22fd5b
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-06-21 20:38:31 +0530

    files added

--- c++ start/V14Array2.cpp
+++ c++ start/V14Array2.cpp
@@ -3,5 +3,17 @@
 #include<vector>
 using namespace std;
 int main(){
-    
+    int arr[]={1,2,3,4,5,4,3,2,1};
+    int ans,check;
+    for(int i=0;i<9;i++){
+        check=0;
+        for(int j=i+1;j<9;j++){
+            if(arr[i]==arr[j]){
+                check=1;
+            }
+        }
+        if(check==0) ans=arr[i];
+    }
+
+    cout<<"unique element is : "<<ans;
 }
