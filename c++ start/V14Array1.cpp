2a4bcefdf6c14ff74f96f1850cf187a2620630f3
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-06-21 19:46:21 +0530

    files added

--- c++ start/V14Array1.cpp
+++ c++ start/V14Array1.cpp
@@ -3,6 +3,19 @@
 #include<vector>
 using namespace std;
 int main(){
+    int arr[]={1,2,3,4,5,6,7,8};
+    int target;
+    cout<<"Enter target : ";
+    cin>>target;
+    int pairs = 0;
+    for (int i = 0; i < 7; i++){
+        for(int j=i+1;j<8;j++){
+            if(arr[i]+arr[j]==target){
+                pairs++;
+            }
+        }
+    }
+    
     
     return 0;
 }
