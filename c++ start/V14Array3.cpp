d58888457a248e64bfecdc64915bdd3ad9de5492
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-06-22 20:56:58 +0530

    files added

--- c++ start/V14Array3.cpp
+++ c++ start/V14Array3.cpp
@@ -3,6 +3,16 @@
 #include<vector>
 using namespace std;
 int main(){
+    int size;
+    cout<<"Enter size of array : ";
+    cin>>size;
+    int arr[size];
+    cout<<"Enter elements of array : ";
+    for (int i = 0; i < size; i++){
+        cin>>arr[i];
+    }
 
+    
+    
     return 0;
 }
