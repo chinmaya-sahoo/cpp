6ea81776403465641c25c71d42886d85db2cc4cf
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-06-24 21:22:30 +0530

    files added

--- c++ start/V14Array5.cpp
+++ c++ start/V14Array5.cpp
@@ -1,7 +1,13 @@
-//Rotate array by k steps...where(may k>size or k<size)
+//Rotate array by k steps...where(may k>size or k<size) with vector
 #include<iostream>
 #include<vector>
 using namespace std;
 int main(){
+    vector<int>v={1,2,3,4,5};
+    int k;
+    cout<<"Enter steps : ";
+    cin>>k;
+    k=k%v.size();
 
+    reverse(v.begin(),v.end());
 }
