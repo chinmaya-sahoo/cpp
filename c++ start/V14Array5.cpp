685e7eb7a2000f2ddf2eef1e310530b5e92a4f64
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-06-24 21:36:18 +0530

    files added

--- c++ start/V14Array5.cpp
+++ c++ start/V14Array5.cpp
@@ -3,12 +3,17 @@
 #include<vector>
 using namespace std;
 int main(){
-    vector<int>v={1,2,3,4,5};
+    vector<int>v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);
     int k;
     cout<<"Enter steps : ";
     cin>>k;
     k=k%v.size();
-
+    
     reverse(v.begin(),v.end());
     reverse(v.begin(),v.begin()+k);
     reverse(v.begin()+k,v.end());
