835d66db7bc508500baf82b8a39072959888bd99
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-06-24 21:44:02 +0530

    files added

--- c++ start/V14Array6.cpp
+++ c++ start/V14Array6.cpp
@@ -1,7 +1,7 @@
-// Given Q queries , check if the given number is present in the array 
-#include<iostream>
+// Given Q queries , check if the given number is present in the array (note: value of all the elements int the array is less than 10 to the power of 5)
+#include<iostream> 
 #include<vector>
 using namespace std;
 int main(){
-    
+
 }
