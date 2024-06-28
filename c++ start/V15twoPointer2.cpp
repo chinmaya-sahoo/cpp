8391dd38250a35a280e62a094fb8d6df84674823
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-06-28 21:25:31 +0530

    files added

--- c++ start/V15twoPointer2.cpp
+++ c++ start/V15twoPointer2.cpp
@@ -6,18 +6,20 @@ void sortByParity(vector<int>&v){
     int left_ptr=0;
     int right_ptr=v.size()-1;
     while(left_ptr<right_ptr){
-        if(left_ptr%2!=0 && right_ptr%2==0){
-            
+        if(v[left_ptr]%2!=0 && v[right_ptr]%2==0){
+            int temp=v[left_ptr];
+            v[left_ptr]=v[right_ptr];
+            v[right_ptr]=temp;
         }
-        if(left_ptr%2==0){
+        if(v[left_ptr]%2==0){
             left_ptr++;
         }
-        }
-        if(right_ptr%2!=0){
+        if(v[right_ptr]%2!=0){
             right_ptr--;
         }
     }
 }
+
 int main(){
     int size;
     cout<<"enter size of array : ";
