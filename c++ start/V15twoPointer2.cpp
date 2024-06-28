db90caeea8910e0282b11586ab61bc8c27bdc60f
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-06-28 21:19:15 +0530

    files added

--- c++ start/V15twoPointer2.cpp
+++ c++ start/V15twoPointer2.cpp
@@ -5,6 +5,20 @@ using namespace std;
 void sortByParity(vector<int>&v){
     int left_ptr=0;
     int right_ptr=v.size()-1;
+    while(left_ptr<right_ptr){
+        if(left_ptr%2!=0 && right_ptr%2==0){
+            swap(left_ptr,right_ptr);
+            left_ptr++;
+            right_ptr--;
+        }
+        if(left_ptr%2==0){
+            left_ptr++;
+        }
+        }
+        if(right_ptr%2!=0){
+            right_ptr--;
+        }
+    }
 }
 int main(){
     int size;
@@ -16,7 +30,7 @@ int main(){
         cin>>v[i];
     }
     sortByParity(v);
-    cout<<"Here is the sorted array : " ;
+    cout<<"Here is the required array : " ;
     for(int i=0;i<size;i++){
         cout<<v[i]<<" ";
     }
