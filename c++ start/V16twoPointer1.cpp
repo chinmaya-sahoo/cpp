c32e6c583bce1cd77e1206db7fd81b280311fbdf
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-06-30 20:56:24 +0530

    files added

--- c++ start/V16twoPointer1.cpp
+++ c++ start/V16twoPointer1.cpp
@@ -1,1 +1,8 @@
 //given an integer array , return prefix sum / running sum in the same array without creating a new array
+#include<iostream>
+#include<vector>
+using namespace std;   
+int main(){
+    
+    return 0;
+}
