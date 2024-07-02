243f26197db74b98d0950d65fd269e1bdf39d693
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-07-02 21:20:41 +0530

    files added

--- c++ start/V16twoPointer2.cpp
+++ c++ start/V16twoPointer2.cpp
@@ -1,7 +1,12 @@
 //check if we can partition the array into two sub-array with equal sum . More formally,check that the prefix sum of a part of the array is equal to the suffix sum of rest of array
-#include<stdio.h>
+#include<iostream>
 #include<vector>
 using namespace std;
 int main(){
+    int size;
+    cout<<"Enter size :";
+    cin>>size;
+    vector<int>v(size);
+    total_sum=0;
     
 }
