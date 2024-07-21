e9f3b872c577400010e5de40f2933b290fa4adc5
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-07-21 17:03:12 +0530

    files added

--- c++ start/V17-2darrays1.cpp
+++ c++ start/V17-2darrays1.cpp
@@ -1,7 +1,12 @@
+// Multi dimensional array --> array[size1][size2][size3]....[sizen]
 #include<iostream>
 #include<vector>
 using namespace std;
 int main(){
-    
+//    int arr[i][j]  --> i = row , j = column 
+    int arr[3][4] = {1,2,3,4,5,6,7,8,9,10,11,12};
+    int arr[3][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+
+// For 3D array eg:- array [2][3][4] == 2 2D arrays of size 3x4
     return 0;
 }
