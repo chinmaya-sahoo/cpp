560785ecf08c73a35721ddb9608730ca69d4ad6f
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-07-22 19:46:54 +0530

    files added

---
+++ c++ start/V18-2Darrays3.cpp
@@ -0,0 +1,15 @@
+// Given a boolean 2D array . where each row is sorted . Find the row with maximum number of 1s .
+// Input :-  
+// row = 3 , col = 4 ;
+// matrix[] = {{0,1,1,1},
+//             {0,0,0,1},
+//             {0,0,0,1}}
+// Output :- 0
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+
+    return 0;
+}
