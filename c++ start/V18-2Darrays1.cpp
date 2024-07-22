f0570db4ef9016045ae4e8f81bdaad89d39eae92
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-07-22 12:24:25 +0530

    files added

---
+++ c++ start/V18-2Darrays1.cpp
@@ -0,0 +1,24 @@
+// study of 2D vectors
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    // initializing vector 
+    // vector<vector<int>>v;
+
+
+    // vector<int>v1={1,4,2};
+    // vector<int>v2={1,9};
+    // vector<int>v3={8,3};
+    // vector<vector<int>>v={v1,v2,v3};
+
+    // int rows,cols;
+    // vector<vector<int>>v(rows,vector<int>(cols));
+
+    // vector<vector<int>>v(3,vector<int>(4,0));    --> A 3x4 vector with elements initialized to zero
+
+
+    
+    
+    return 0;
+}
