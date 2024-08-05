e2df08acd2f3900e05246d20af3b1117c2e60c65
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-08-05 21:23:09 +0530

    first commit

---
+++ c++ start/V20spiral_matrix.cpp
@@ -0,0 +1,9 @@
+// given an n x m matrix ,return all elementy in spiral order
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std; 
+int main(){
+    
+    return 0;
+}
---
+++ c++ start/new.cpp
@@ -0,0 +1,6 @@
+#include<iostream>  
+using namespace std;
+int main(){
+    int arr[]={1,2,3,4,5,6,7,8};
+    cout<<sizeof(arr);
+}
