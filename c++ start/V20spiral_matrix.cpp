3223a1e149779a217e188ac2b1419307969e5047
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-08-16 13:07:02 +0530

    updated

--- c++ start/V20spiral_matrix.cpp
+++ c++ start/V20spiral_matrix.cpp
@@ -4,20 +4,30 @@
 #include<algorithm>
 using namespace std; 
 int main(){
-    vector<vector<int>>v(n,vector<int>(n));
+    vector<vector<int>>v(m,vector<int>(n));
     cout <<"Enter the elements of the vector :";
     for (int i = 0; i < v.size(); i++){
         for (int j = 0; j < v[0].size(); j++){
             cin >> v[i][j] ; 
         }
     }
-
-
-
     
     for (int i = 0; i < v.size(); i++){
+        // right to left 
         for (int j = 0; j < v[0].size(); j++){
-            cin >> v[i][j] ; 
+            cout >> v[i][j] ; 
+        }
+        // top to bottom 
+        for (int j = 0; j < v[0].size(); j++){
+            cout >> v[i][j] ; 
+        }
+        // left to right 
+        for (int j = 0; j < v[0].size(); j++){
+            cout >> v[i][j] ; 
+        }
+        // bottom to top 
+        for (int j = 0; j < v[0].size(); j++){
+            cout >> v[i][j] ; 
         }
     }
     
