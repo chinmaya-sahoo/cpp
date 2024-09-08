73cfeb09ed65b53048bfd54749e0de18a06f9ba7
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-09-08 21:32:32 +0530

    files added

--- OOPS/oops5.cpp
+++ OOPS/oops5.cpp
@@ -1,6 +1,5 @@
 // type of constructor (i)Non-Parameterized constructor , (ii) parameterized constructor , (iii) copy constructor
 
-
 #include<iostream>
 #include<string>
 using namespace std;
@@ -14,17 +13,34 @@ class Teacher{
     string dept;
     string subject;
 
-    Teacher(string name, string dept, string subject){
+// parameterized constructor
+
+    // Teacher(string n, string d, string s){
+    //     cout << "constructor is called" << endl;
+    //     name = n ;
+    //     dept = d;
+    //     subject = s;
+
+
+    // this->name = name ;     // (*this).name = name ;
+    // this->dept = dept;
+    // this->subject = subject;
+
+
+    // }
+
+
+// non-parameterized constructor
+
+    Teacher(){
         cout << "constructor is called" << endl;
-        this->name = name ;
-        this->dept = dept;
-        this->subject = subject;
     }
 };
 
 int main(){
-    Teacher t1("chinmaya", "CSE" , "DSA");
+    // Teacher t1("chinmaya", "CSE" , "DSA");
+    Teacher t2;
 
-    cout<< t1.dept << endl;
+    // cout<< t1.dept << endl;
     return 0;
 }
