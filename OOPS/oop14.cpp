908d82b8bf0f8ba832d975ebc9a9dad95b977595
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-09-13 20:23:34 +0530

    files added

--- OOPS/oop14.cpp
+++ OOPS/oop14.cpp
@@ -1,7 +1,30 @@
 // Polymorphism is the ability of objects to take on 'different forms' or behaves in differently ways 'depending on the context' in which they are used.
 
+
+//  --> Compile Time Polymorphism 
+//  --> Run Time Polymorphism
+
+
 #include<iostream>
 using namespace std;
-int main(){
+class student{
+    public:
+    string name;
+
+    student(){
+        cout<<"non-parameterized"<<endl;
+    }
+
+    student(string name){
+        cout<<"parameterized"<<endl;
+        this->name = name;
+    }
     
+};
+int main(){
+    student s1;
+    student s2("John");
+
+    
+    return 0;
 }
