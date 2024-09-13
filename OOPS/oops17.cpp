4d4cc47ce667eb6afa8511029640f10495bc2640
chinmay sahoo <chinmaysahoo668@gmail.com>
2024-09-13 21:10:21 +0530

    files added

--- OOPS/oops17.cpp
+++ OOPS/oops17.cpp
@@ -7,9 +7,25 @@
 // -> A virtual function is called during runtime
 
 
+
+
+
+
+
+
 // 'Abstraction' :- Hiding all unnecessary details & showing only the important parts 
 
 // -> also known as 'access specifiers' / 'access modifiers' 
+
+// using abstract classes -->
+//  -> Abstract classes are used to provide a base class from which other classes can be derived
+//  -> They can't be instantiated and are meant to be inherited .   ( 'instance' -> 'object')
+//  -> Abstract classes are typically used to define an interface for derived classes .
+
+
+
+
+
 
 #include<iostream>
 using namespace std;
